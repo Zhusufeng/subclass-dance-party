2cab5ce3767d5c6667b712fb891208bf490d29fc
Lisa Gee <ZhuSuFeng@gmail.com>
2017-07-10 20:42:29 -0700

    Update Dancer constructor and methods

--- src/dancer.js
+++ src/dancer.js
@@ -8,24 +8,28 @@ var makeDancer = function(top, left, timeBetweenSteps) {
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
   // this one sets the position to some random default point within the body
   // dancer.setPosition(top, left);
-  this.setPosition(top, left);
+  this.setPosition(this.top, this.left);
+  this.top = top;
+  this.left = left;
+  this.timeBetweenSteps = timeBetweenSteps;
 };
 
 makeDancer.prototype.step = function() {
   // the basic dancer doesn't do anything interesting at all on each step,
   // it just schedules the next step
-  setTimeout(this.step, timeBetweenSteps);
+  //console.log('dancer.js step func: ', this.step);
+  setTimeout(this.step.bind(this), this.timeBetweenSteps);
 };
 
 
 
-makeDancer.prototype.setPosition = function(top, left) {
+makeDancer.prototype.setPosition = function() {
   // Use css top and left properties to position our <span> tag
   // where it belongs on the page. See http://api.jquery.com/css/
   //
   var styleSettings = {
-    top: top,
-    left: left
+    top: this.top,
+    left: this.left
   };
   this.$node.css(styleSettings);
 };
